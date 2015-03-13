0811efec1db3189cea9f4bd3629a44e98ef08bff
Jim Buck <jim@jimmyboh.com>
2015-03-13 14:57:14 -0400

    Create app.js

---
+++ app/app.js
@@ -0,0 +1,15 @@
+
+var socket = io.connect('http://localhost:3000');
+
+socket.on('connect', function () {
+  // socket connected
+  socket.emit('forward', {
+    action : 'message',
+    data : {
+      string : 'thing'
+      number : 10
+    }
+  });
+  
+  socket.close();
+});
